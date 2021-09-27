0160a6813d000b69be3a0e46d3f2d92857bd7423
chosh <chotjd329@hotmail.com>
2021-09-27 17:10:47 +0900

    feat: util 기능 추가
    utils 전역객체에 붙이는 예시
    utils alias 적용

--- client/src/app.js
+++ client/src/app.js
@@ -1,10 +1,16 @@
 import "_styles/style.scss";
 import logo from "_images/logo.svg";
 import profilePng from "_images/profile.png";
+import utils from "_utils/utils.js";
 import MainService from "./mainService.js";
 
+(function setGlobalMethod() {
+  window.$ = utils.$;
+  window.$$ = utils.$$;
+})();
+
 window.addEventListener("DOMContentLoaded", () => {
-  const targetEl = document.querySelector("#log");
+  const targetEl = $("#log");
 
   const service = new MainService({ targetEl });
   const datalist = [1, 2, 3, 4, [5, 6, [7]]];
