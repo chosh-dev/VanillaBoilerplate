377538e29517cbba706b12e255220803914eec61
chosh <chotjd329@hotmail.com>
2021-12-24 11:08:22 +0900

    🔥 eslint 인식 문제로 전역변수 $ 제거

--- client/src/app.js
+++ client/src/app.js
@@ -1,12 +1,6 @@
 import { initRouter } from '_core/router';
 import { initStore } from '_store/';
-import utils from '_utils/utils';
 import '_styles/global.scss';
-
-(function setGlobalMethod() {
-  window.$ = utils.$;
-  window.$$ = utils.$$;
-})();
 
 window.addEventListener('DOMContentLoaded', () => {
   initStore();
