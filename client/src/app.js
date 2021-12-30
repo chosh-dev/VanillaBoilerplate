dde2e0656c99f06da5d35e295a32dd527e6b2e59
chosh <chotjd329@hotmail.com>
2021-12-30 14:38:03 +0900

    ✨ reset scss reset css로 변경 및 적용

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,7 @@
 import { initRouter } from '_core/router';
 import { initStore } from '_store/';
 import '_styles/global.scss';
+import '_styles/reset.css';
 
 window.addEventListener('DOMContentLoaded', () => {
   initStore();
